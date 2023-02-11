598d6839ddfce811b69ad3772751509ddc234aae
nyarosu <ronak.chatterjee65@gmail.com>
2023-02-11 17:28:11 +0000

    fix small errors

--- routes/get.controller.ts
+++ routes/get.controller.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from 'express';
+import { request } from 'http';
 
 function getRocket(Request: Request, Response: Response) {
+    if (Request.headers.wallet_address) {
+      const wallet_address = Request.headers.wallet_address[0];
+      getListAssets(wallet_address, 'name')
+      .then((result) => {
+        //print the result
+        console.log(result);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    }
     
-    const wallet_address = Request.header.wallet_address;
-
-    getListAssets(wallet_address, 'name')
-    .then((result) => {
-      //print the result
-      console.log(result);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-
 }
 
 export {
