feb9617b6d4dfb7747ffb3a02d20469391d84d9b
nyarosu <ronak.chatterjee65@gmail.com>
2023-02-11 17:16:10 +0000

    implement rocket creation

--- routes/create.controller.ts
+++ routes/create.controller.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from 'express';
 
-function createRocket(Request: Request, Response: Response) {
+interface METADATA_TEMPLATE {
+    id: number,
+    name: string,
+    description: string,
+    image_url: string 
+    speed: number,
+    health: number,
+    laserstrength: number,
+    fuel: number,
+    rating: number,
+}
+
+// Call with a wallet address to receive a blank, level 1 rocket.
+function createRocket(req: Request, res: Response) {
 
 }
 
--- routes/create.router.ts
+++ routes/create.router.ts
@@ -3,6 +3,7 @@ import { createRocket } from './create.controller';
 
 const createRouter = Router();
 
+// Create and receive a new rocket
 createRouter.post('/rocket', createRocket)
 
 export default createRouter
